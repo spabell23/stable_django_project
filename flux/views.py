e0e7a902a5219130c0b60aab1907695c45923379
Kevin Wojton <your.kfwojton@gmail.com>
2018-03-07 09:29:04 -0500

    initialized templates

---
+++ flux/views.py
@@ -0,0 +1,13 @@
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+
+    template_name = "index.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['latest_articles'] = Article.objects.all()[:5]
+        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['latest_articles'] = Article.objects.all()[:5]
+    #     return context
