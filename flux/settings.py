5e4c62461e864dc0e956a77865189651bb14790e
Kevin Wojton <your.kfwojton@gmail.com>
2018-03-25 13:18:27 -0400

    aded allowed_hosts

--- flux/settings.py
+++ flux/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7fd5^_bgzmq!8!-7zm=b*w3jfgk4bkn!0i+hfx(3mh!$!+pqan'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clevelandrocksnew.herokuapp.com','localhost:8000','clevelandrocksclimbing.com']
 
 
 # Application definition
